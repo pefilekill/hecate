4cf3a17e8ac1609bb12032cdfa17af4c27a133d2
wangwenzhe <wangwenzhe@wangwenzhes-MacBook-Pro.local>
2017-02-27 15:52:35 +0800

    test

---
+++ CPlusPlus/Executable/Loader/cmake-build-debug/CMakeFiles/Loader.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Loader.dir/main.cpp.o"
+  "Loader.pdb"
+  "Loader"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Loader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CPlusPlus/Executable/Loader/cmake-build-debug/CMakeFiles/Loader.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Loader.
+# This may be replaced when dependencies are built.
